fd8f947eed11826d65afef191c076f920d5049fc
wufan8387 <ccbw@163.com>
2019-06-13 22:37:39 +0800

    initialize

---
+++ src/main/java/org/smart/orm/App.java
@@ -0,0 +1,16 @@
+package org.smart.orm;
+
+import java.util.function.Function;
+
+/**
+ * Hello world!
+ */
+public class App {
+    class X {
+        public String Q;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
